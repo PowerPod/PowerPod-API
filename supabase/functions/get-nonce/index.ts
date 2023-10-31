caf75815e7a6d5a265a3ce6be3295435631d142e
bob <>
2023-10-31 23:37:36 +0800

    verify address

--- supabase/functions/get-nonce/index.ts
+++ supabase/functions/get-nonce/index.ts
@@ -1,6 +1,7 @@
 import { generateNonce as getNonce } from 'siwe'
 import { corsHeaders } from '../_shared/cors.ts'
 import { supabaseAdmin } from '../_shared/supabaseAdmin.ts'
+import { isAddress } from '@ethersproject/address'
 
 Deno.serve(async (req) => {
   if (req.method === 'OPTIONS') {
@@ -12,7 +13,9 @@ Deno.serve(async (req) => {
 
     if (!publicAddress) throw new Error('missing params')
 
-    // TODO: vrify a valid publicAddress
+    if (!isAddress(publicAddress)) {
+      throw new Error('Invalid public address')
+    }
 
     const { data: fetchData, error: fetchError } = await supabaseAdmin
       .from('t_nonces')
